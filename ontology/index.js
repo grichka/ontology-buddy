f02fbab269d2cecc659a3aabfda3661b4767a6c4
Thomas Moreau <thomas.moreau.f@gmail.com>
2023-02-19 16:37:23 +0100

    ✨ Implement cardinality in Mermaid classDiagram

--- ontology/index.js
+++ ontology/index.js
@@ -5,7 +5,7 @@ const model = require('./model.js');
  * @param {string} ontologyFile Content of an OWL ontology file
  * @param {*} format Format (default: `Turtle`)
  * @param {*} baseIri Ontology base IRI
- * @returns {import("./model.js").ClassHierarchy}
+ * @returns {Promise<import("./model.js").ClassHierarchy>}
  */
 async function getClassHierarchy(ontologyFile, baseIri, format = 'Turtle') {
   return model.getClassHierarchy(await parser.parse(ontologyFile, baseIri, format));
